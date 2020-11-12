e729d7bd9abde4aa408a6ce9ee1f6efc8ac61516
DiogoVF <diogof-6@hotmail.com>
2020-11-12 10:51:50 +0100

    change button to textual object

--- core/src/com/mygdx/game/models/Button.java
+++ core/src/com/mygdx/game/models/Button.java
@@ -3,7 +3,7 @@ package com.mygdx.game.models;
 
 import com.badlogic.gdx.math.Vector2;
 
-public class Button extends PhysicalObject {
+public class Button extends TextualObject {
     private String name;
 
     public Button(String picname, String name, Vector2 position, float width, float height) {
